b22b63a7c9266034bc15a492c99a82d54db208d4
“crlnvls” <cveloso@hotmail.co.uk>
2023-02-23 21:12:09 +0000

    Add firebase auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+
 import NavBar from "./components/NavBar";
 import ChatBox from "./components/ChatBox";
 import Welcome from "./components/Welcome";
 
+import { auth } from "./firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+
 const App = () => {
-  const [user, setUser] = useState(false);
+  const [user] = useAuthState(auth);
 
   return (
     <>
