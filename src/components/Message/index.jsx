bd802c8ffdfc7bfc820c96931916fe93de2c62cd
“crlnvls” <cveloso@hotmail.co.uk>
2023-03-09 22:26:18 +0000

    Add temporarily css

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -3,13 +3,20 @@ import React, { useEffect, useState } from "react";
 import { auth } from "../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
-const Message = () => {
+const Message = ({ message }) => {
+  const [user] = useAuthState(auth);
+
   return (
     <>
-      <div>
-        <div>
-          <p>Caroline Veloso</p>
-          <p>Enjoy this real time chat</p>
+      <div className={`chat-bubble ${message.uid === user.uid ? "right" : ""}`}>
+        <img
+          className="chat-bubble__left"
+          src={message.avatar}
+          alt="user avatar"
+        />
+        <div className="chat-bubble__right">
+          <p>{message.name}</p>
+          <p>{message.text}</p>
         </div>
       </div>
     </>
