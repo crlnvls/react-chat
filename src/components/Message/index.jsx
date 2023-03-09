6b6bdbe81d5d5a331a5830b2e3db9bddba072ab5
“crlnvls” <cveloso@hotmail.co.uk>
2023-03-09 21:57:22 +0000

    Update imports

--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -1,14 +1,7 @@
 import React, { useEffect, useState } from "react";
 
-import {
-  query,
-  collection,
-  orderBy,
-  onSnapshot,
-  limit,
-} from "firebase/firestore";
-
-import { db } from "../firebase";
+import { auth } from "../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 const Message = () => {
   return (
